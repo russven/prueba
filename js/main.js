32f5379db0b4edc4ff3f74cab63368b04aca9aee
russven <r.estiven98@gmail.com>
2025-04-22 18:18:56 +0200

    Agregar archivos iniciales de HTML, CSS y JavaScript para el proyecto Taper'fecto

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function verCategoria(nombre) {
+    alert(`Has seleccionado la categoría: ${nombre}`);
+  }
+  
+  document.getElementById("registerBtn").addEventListener("click", function () {
+    alert("Aquí iría el formulario de registro (próximamente)");
+  });
